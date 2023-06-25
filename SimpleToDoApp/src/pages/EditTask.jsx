773d78686c804f44c53f7c3ec79649a658f88eae
Rafael de Figueiredo Alves <rafael.figueiredo.alves@gmail.com>
2023-06-25 19:03:23 -0300

    Create EditTask.jsx

---
+++ SimpleToDoApp/src/pages/EditTask.jsx
@@ -0,0 +1,11 @@
+import { Formulario } from '../shared/Formulario';
+
+export const EditTask = () => {
+    return(
+        <>
+        <h3>Editando Tarefa</h3>
+
+        <Formulario />
+        </>
+    )
+}
