653cacf24334f7cdd9a783b68bbecc3a5efd77e8
Rafael de Figueiredo Alves <rafael.figueiredo.alves@gmail.com>
2024-03-31 22:15:03 -0300

    Update TasksContext.jsx

--- SimpleToDoApp/src/Context/TasksContext.jsx
+++ SimpleToDoApp/src/Context/TasksContext.jsx
@@ -13,17 +13,19 @@ export const TasksContext = createContext(
 );
 
 export const TasksProvider = (props) => {
-    const [Tasks, setTasks] = useState(LerBD);
+    const [Tasks, setTasks] = useState([]);
 
-    useEffect(() => GravarBD(Tasks), [Tasks])
+     useEffect(() => setTasks(() => LerBD()), [])
 
     const AddTask = (tarefa, descricao) => {
         const new_task = Tarefa.NewTask(tarefa, descricao)
         setTasks([...Tasks, new_task]);
+        GravarBD(Tasks);
     }
 
     const RemoveTask = (id) => {
         setTasks(() => Tasks.filter((task) => task.id !== id));
+        GravarBD(Tasks);
     }
 
     const UpdateTask = (tarefa) => {
@@ -31,14 +33,16 @@ export const TasksProvider = (props) => {
        const index = Tasks.indexOf(task);
        Tasks[index].tarefa = tarefa.tarefa;
        Task[index].descricao = tarefa.descricao;
-       //Task[index].feito = tarefa.feito;
+       Task[index].feito = tarefa.feito;
        setTasks([...Tasks]);
+       GravarBD(Tasks);
     }
 
     const toggleTask = (task) => {
         const index = Tasks.indexOf(task);
         Tasks[index].feito = !task.feito;
         setTasks([...Tasks]);
+        GravarBD(Tasks);
     }
 
     return <TasksContext.Provider value={{Tasks, AddTask, RemoveTask, toggleTask, UpdateTask}}>{props.children}</TasksContext.Provider>;
